79e1df7c3091e8ff5231bb9a7e5673c02af2c0e1
Oksana-ua2020 <mirisa5555@gmail.com>
2020-11-06 17:56:04 +0200

    DZ type

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
-    console.log('Hello World');    
+    "use strict"
+    console.log('Hello World');  
+    
+    let name ="John";
+    let age16 = true;
+    let numCard = 25
+    let maxPrice = 1000200030004000500060007000n;
+    let strUndefined = "undefined";
+    let strNull = "'null'";
+    let sex;
+    let city = null;
+    console.log(
+        `${typeof(name)} - ${name}, ${typeof(age16)} - ${age16}, ${typeof(numCard)} - ${numCard}, `
+        + typeof(maxPrice) + ' - ' + maxPrice + ', "' + strUndefined + '" - ' + typeof(sex) + ', '  + strNull + ' - ' + city);
 
