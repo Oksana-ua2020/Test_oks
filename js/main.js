af623341ad727cd95149c1fd12df625864253d91
Oksana-ua2020 <mirisa5555@gmail.com>
2020-11-09 17:49:43 +0200

    option2

--- js/main.js
+++ js/main.js
@@ -8,16 +8,10 @@
         let city = null;
     console.log(
         `${typeof(name)} - ${name}, ${typeof(age16)} - ${age16}, ${typeof(numCard)} - ${numCard}, `
-        + typeof(maxPrice) + ' - ' + maxPrice + ', "undefined" - ' + typeof(sex) + ', '  + "'null'"  + ' - ' + city);
+        + typeof(maxPrice) + ' - ' + maxPrice + ', "undefined" - ' + sex + ', '  + "'null'"  + ' - ' + city);
 
-    console.log('option 2');
-        let nameUser ="John";
-        let age16User = true;
-        let numCardUser = 25
-        let maxPriceLast = 1000200030004000500060007000n;
-        let sexUser;
-        let cityUser = null;
-    console.log(
-        `${"string"} - ${nameUser}, ${"boolean"} - ${age16User}, ${"number"} - ${numCardUser}, `
-        + 'bigint - ' + maxPriceLast + ', "undefined" - ' + sexUser + ', ' + "'null'" + ' - ' + cityUser);
+    console.log('option 2');  
+        console.log(
+            `${"string"} - ${name}, ${"boolean"} - ${age16}, ${"number"} - ${numCard}, `
+            + 'bigint - ' + maxPrice + ', "undefined" - ' + sex + ', ' + "'null'" + ' - ' + city);
  
