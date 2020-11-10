356306f621593cdceff7ad52735ab4fe83b9b3fe
Oksana-ua2020 <mirisa5555@gmail.com>
2020-11-10 15:26:39 +0200

    options

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
     "use strict"
-    console.log('option 1');  
         let name ="John";
         let age16 = true;
-        let numCard = 25
+        let numCard = 25;
         let maxPrice = 1000200030004000500060007000n;
         let sex;
         let city = null;
-    console.log(
+
+    console.log('option 1');  
+        console.log(
         `${typeof(name)} - ${name}, ${typeof(age16)} - ${age16}, ${typeof(numCard)} - ${numCard}, `
-        + typeof(maxPrice) + ' - ' + maxPrice + ', "undefined" - ' + sex + ', '  + "'null'"  + ' - ' + city);
+        + typeof(maxPrice) + ' - ' + maxPrice + ', "undefined" - ' + typeof(sex) + ', '  + "'null'"  + ' - ' + city);
 
     console.log('option 2');  
         console.log(
